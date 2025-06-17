ae395bb488c5b369b5dae58cbc7ca2a463c5e56a
Nayane Rios <nayanerodriguesrios@gmail.com>
2025-06-17 18:38:22 -0300

    Corrigido erro de digitação no arquivo README

--- livraria_db.sql
+++ livraria_db.sql
@@ -1,5 +1,5 @@
 -- =============================================================================
--- Este script cria as tabelas e insere os dados de exemplo.
+-- Este script cria as tabelas e insere os dados de exemplo do enunciado.
 -- Resolução Atividade checkout projeto integrador 2
 -- =============================================================================
 
